b28e47609065f17c5d8073875b382c3da8184906
André Luft <a.luft1976@gmail.com>
2022-05-20 16:16:59 +0200

    text_index.html

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -10,71 +10,62 @@ class ShoppingList extends React.Component {
 		return (
 			//füge hier deinen HTML-Code ein
 			<div id="container">
-				<header/>
+				{/* ToDo: füge hier drunter Deinen HTML-Code ein*/ }
+				
+				<header>
+					<h1>Einkaufsliste</h1>
+					<nav>
+						<input type="text" placeholder="Artikel hinzufügen"/>
+						<button className="material-icons">add_circle</button>
+					</nav>
+				</header>
 				
 				<hr/>
-				
 				<main>
-					<section/>
-					<h2>Einkaufen
-						<i style="float:right" class="material-icons">expand_less</i>
-					</h2>
-					<dl>
-						<dt style="background-color: lightblue"/>
-						Obst & Gemüse
-						<i style="float:right" class="material-icons"/>expand_less<i/>
-						<dt/>
-						<dd><label><input type="checkbox"> Brokkoli<label/>
-							<dd/>
-							<dt style="background-color: lightblue">Getreideprodukte
-								<i style="float:right" class="material-icons">expand_less</i>
+					<section>
+						<h2>Einkaufen
+							<i className="material-icons">expand_less</i>
+						</h2>
+						<dl>
+							<GruppenTag/>
+						</dl>
+					</section>
+					<hr/>
+					<section>
+						<h2>Erledigt
+							<i className="material-icons">expand_less</i>
+						</h2>
+						<dl>
+							<dt>Hülsenfrüchte
+								<i className="material-icons">expand_less</i>
 							</dt>
-							<dd><label><input type="checkbox"> Reis<label/>
-								<dd/>
-								<dt style="background-color: lightblue">Milchprodukte
-									<i style="float:right" class="material-icons">expand_less</i>
-								</dt>
-								<dd><label><input type="checkbox"> Streukäse<label/>
-									<dd/>
-									<dl>
-										<section/>
-										<hr>
-											<section>
-												<h2>Erledigt
-													<i style="float:right" class="material-icons">expand_less</i>
-												</h2>
-												<dl>
-													<dt style="background-color: lightblue">Hülsenfrüchte
-														<i style="float:right" class="material-icons">expand_less</i>
-													</dt>
-													<dd><label><input type="checkbox" checked> <s>Tofu</s><label/>
-														<dd/>
-														<dl>
-															<section/>
-															<main/>
-															<hr>
-																
-																<footer>
-																	<nav>
-																		<button>
-																			<span
-																				class="material-icons">bookmark_add</span> Gruppen
-																		</button>
-																		<button>
-																			<span
-																				class="material-icons">sort</span> Sortieren
-																		</button>
-																		<button>
-																			<span
-																				class="material-icons">settings</span> Einstellungen
-																		</button>
-																	</nav>
-																</footer>
-																Füge hier Deinen HTML-Code ein<br/>
-																<GruppenTag/>
-																<div/>
-																)
-																}
-																}
-																
-																export default ShoppingList
+							<dd><label><input type="checkbox"/> <s>Tofu</s></label></dd>
+						</dl>
+					</section>
+					
+				</main>
+				<hr/>
+				<footer>
+					<button className="mdc-button mdc-button--raised">
+						<span className="material-icons">bookmark_add</span>
+						<span className="mdc-button__ripple"></span> Gruppen
+					</button>
+					<button className="mdc-button mdc-button--raised">
+						<span className="material-icons">sort</span>
+						<span className="mdc-button__ripple"></span> Sort
+					</button>
+					<button className="mdc-button mdc-button--raised">
+						<span className="material-icons">settings</span>
+						<span className="mdc-button__ripple"></span> Setup
+					</button>
+				</footer>
+			</div>
+	)
+	}
+	}
+	
+	export default ShoppingList
+	
+	
+	
+	
--- src/components/ArtikelTag.js
+++ src/components/ArtikelTag.js
@@ -9,7 +9,7 @@ class ArtikelTag extends React.Component {
     return (
       <React.Fragment>
         {/* ToDo: füge hier drunter Deinen HTML-Code ein */}
-        Füge hier Deinen ArtikelTag-Code<br/>
+        <br/>
       </React.Fragment>
     )
   }
